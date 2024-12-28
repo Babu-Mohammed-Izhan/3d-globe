764aee85f2d8b08fdd878a2cbe6f047026047c62
Babu Izhan <babu.izhan@shopup.org>
2024-12-28 17:19:51 +0530

    update ball graphics

--- src/glossy-sphere/index.tsx
+++ src/glossy-sphere/index.tsx
@@ -1,20 +1,39 @@
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import GlossySphere, { Grading } from "./sphere";
+import GlossySphere from "./sphere";
+import { useControls } from "leva";
+import { PresetsType } from "@react-three/drei/helpers/environment-assets";
 
 const GlossySphereCanvas = () => {
+  const { environment } = useControls({
+    environment: {
+      value: "lobby",
+      options: [
+        "apartment",
+        "city",
+        "dawn",
+        "forest",
+        "lobby",
+        "night",
+        "park",
+        "studio",
+        "sunset",
+        "warehouse",
+      ],
+    },
+  });
+
   return (
     <Canvas frameloop="demand" camera={{ position: [0, 0, 5], fov: 45 }}>
-      <ambientLight />
+      <ambientLight intensity={6} />
       <spotLight
-        intensity={0.5}
+        intensity={2}
         angle={0.2}
         penumbra={1}
         position={[5, 15, 10]}
       />
       <GlossySphere />
-      <Grading />
-      <Environment preset="dawn" background blur={0.6} />
+      <Environment preset={environment as PresetsType} background />
       <OrbitControls enableZoom={false} />
     </Canvas>
   );
--- src/glossy-sphere/sphere.tsx
+++ src/glossy-sphere/sphere.tsx
@@ -1,28 +1,6 @@
-import { extend, ReactThreeFiber, useLoader } from "@react-three/fiber";
-import { Effects, useTexture } from "@react-three/drei";
-import { LUTPass, LUTCubeLoader } from "three-stdlib";
+import { useTexture } from "@react-three/drei";
 import { useState } from "react";
 import { Texture } from "three";
-
-extend({ LUTPass });
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      lUTPass: ReactThreeFiber.Object3DNode<LUTPass, typeof LUTPass>;
-    }
-  }
-}
-
-export function Grading() {
-  const cubeResult = useLoader(LUTCubeLoader, "/cubicle-99.CUBE");
-
-  return (
-    <Effects>
-      <lUTPass lut={cubeResult.texture3D} intensity={0.75} />
-    </Effects>
-  );
-}
 
 const GlossySphere = () => {
   const texture = useTexture("/terrazo.png");
@@ -32,9 +10,9 @@ const GlossySphere = () => {
     <mesh onClick={() => setClicked((prev) => !prev)}>
       <sphereGeometry args={[1, 64, 64]} />
       <meshPhysicalMaterial
-        map={clicked ? new Texture() : texture} // Reset texture on click
+        map={clicked ? texture : new Texture()}
         clearcoat={1}
-        metalness={0.5}
+        metalness={1}
         clearcoatRoughness={0}
         roughness={0}
       />
